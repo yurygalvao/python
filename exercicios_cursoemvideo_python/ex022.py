4cb0f4815b7d3aea7b7e16c037c6331a89f790c2
Yury Galvão <yurygaraujo@gmail.com>
2024-10-01 10:58:04 -0300

    .

---
+++ exercicios_cursoemvideo_python/ex022.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print(f'Seu nome em maiúsculas é {nome.upper()}')
+print(f'Seu nome em minúsculas é {nome.lower()}')
+print('Seu nome ao todo tem {} letras'.format(len(nome) - nome.count(' ')))
+separa = nome.split()
+print(f'Seu primeiro nome é {separa[0]} e tem {len(separa[0])} letras')
